d4ef6f09598b354c08bdb388be193ca4ab655957
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-04-28 09:01:38 +0200

    added navbar & hamburger menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import NavBar from './components/userPanel/NavBar/NavBar'
+
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/userPanel/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NavBar.scss'
+interface Props {
+
+}
+
+const NavBar = (props: Props) => {
+    return (
+        <div className='navbar-container'>
+            <div className='logo'>Burger Queen</div>
+        </div>
+    )
+}
+
+export default NavBar
