d05c3dac81c2e2e468d66bc240d5a1a6ea60a787
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-03 07:56:02 +0200

    finished header

--- src/components/userPanel/homePage/Header/Header.tsx
+++ src/components/userPanel/homePage/Header/Header.tsx
@@ -9,8 +9,8 @@ const Header = (props: Props) => {
         <div className='header'>
             <p className='yum'>yum yum!</p>
             <div className="text-container">
-                <p className='mission'>our mission</p>
-                <h1 className='header-text'>is to challenge the way people think about burgers. We’re about creative construction. We think life is about experiences. Trying stuff. Learning. Being Inventive.</h1>
+                <p className='mission'>Serving more than burgers since 1985.</p>
+                {/* <h1 className='header-text'>Serving more than burgers since 1985.</h1> */}
             </div>
         </div >
     )
